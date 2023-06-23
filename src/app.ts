d0d63fd45c2daedb0a6aeb2c69d2f945bf4f633f
fahimrimon <fahimrimon21@gmail.com>
2023-06-23 18:26:59 +0600

    husky and lint stage setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app:Application = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
 // use cors
 app.use(cors());
 // use parse
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Testing
-app.get('/', (req:Request, res:Response) => {
-  res.send('Working successfully')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working successfully");
+});
 
 export default app;
